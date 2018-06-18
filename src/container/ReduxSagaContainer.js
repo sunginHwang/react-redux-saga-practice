e6480aa2f8246bc36785f0983d5e73545df7faa5
HwangSungIn <gommpo111@gmail.com>
2018-06-18 18:45:46 +0900

    -- 작업내역
    * redux-saga + ducks 구조로 saga의 async 콜 정상 동작 처리
    [request -> success or fail]
    
    - 해야 할 사항
    
    * async 콜에 대한 saga 콜링을 공통화로 빼보자
    * auth 처리에 대한 고민 필요

--- src/container/ReduxSagaContainer.js
+++ src/container/ReduxSagaContainer.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as ReduxSagaExampleActions from '../saga/modules/ReduxSagaExample';
 import ReduxSagaComponent from '../component/ReduxSagaComponent';
 
 class ReduxSagaContainer extends Component {
 
+    successBtnClick(){
+        const { ReduxSagaExampleActions } = this.props;
+        ReduxSagaExampleActions.asyncRequest(`https://jsonplaceholder.typicode.com/posts/1`);
+    }
+
+    failBtnClick(){
+        const { ReduxSagaExampleActions } = this.props;
+        ReduxSagaExampleActions.asyncRequest(`https://jsonplaceholder.typicode.com/3232posts/1`);
+    }
   render() {
+      const { count, title, body } = this.props;
     return(
      <div>
          <ReduxSagaComponent/>
-         <div>redux count is <span> ${this.props.count}`</span></div>
+         <div>redux status is <span> {count}</span></div>
+         <div>redux body is <span> {body}</span></div>
+         <div>redux title is <span> {title}</span></div>
+         <div>
+             <button onClick={(e)=>{
+                 this.successBtnClick()}}>apiSuccessCallButton</button>
+             <button onClick={(e)=>{
+                 this.failBtnClick()}}>apiFailCallButton</button>
+         </div>
      </div>
     );
   }
 }
 export default connect(
     (state) => ({
-        count: state.ReduxSagaExampleReducer.count
+        count: state.ReduxSagaExampleReducer.count,
+        body: state.ReduxSagaExampleReducer.body,
+        title: state.ReduxSagaExampleReducer.title
     }),
-    (dispatch) => ({})
-)(ReduxSagaComponent);
+    (dispatch) => ({
+        ReduxSagaExampleActions: bindActionCreators(ReduxSagaExampleActions, dispatch),
+    })
+)(ReduxSagaContainer);
--- src/saga/modules/ReduxSagaExample.js
+++ src/saga/modules/ReduxSagaExample.js
@@ -1,26 +1,36 @@
 import { createAction, handleActions } from 'redux-actions';
 import { fork, take, call, put, cancel } from 'redux-saga/effects';
-
+import axios from 'axios';
 const ASYNC_REQUEST = "ReduxSagaExample/ASYNC_REQUEST";
 const ASYNC_REQUEST_SUCCESS = "ReduxSagaExample/ASYNC_REQUEST_SUCCESS";
 const ASYNC_REQUEST_FAILURE = "ReduxSagaExample/ASYNC_REQUEST_FAILURE";
 
-const asyncRequest = createAction(ASYNC_REQUEST);
-const asyncRequestSuccess = createAction(ASYNC_REQUEST_SUCCESS);
-const asyncRequestFailure = createAction(ASYNC_REQUEST_FAILURE);
+
+
+const getPostAPI =  (url) => {
+     return axios.get(url);
+};
+
+export const asyncRequest = createAction(ASYNC_REQUEST);
+export const asyncRequestSuccess = createAction(ASYNC_REQUEST_SUCCESS);
+export const asyncRequestFailure = createAction(ASYNC_REQUEST_FAILURE);
+
 
 const initialState = {
     count: 0,
-    status: ''
+    status: '',
+    title: '',
+    body: ''
 };
 
 export default function * asyncRequestSaga(info) {
     while(true) {
         const action = yield take(ASYNC_REQUEST);
-        const {data} = action;
+        const { data, payload } = action;
+
         try {
-            const json = yield call(null);
-            const successAction = asyncRequestSuccess(json,12);
+            const json = yield call(getPostAPI,payload);
+            const successAction = asyncRequestSuccess(json);
             yield put(successAction)
         } catch(error) {
             const failureAction = asyncRequestFailure(error);
@@ -34,10 +44,10 @@ export const ReduxSagaExampleReducer =  handleActions({
         return { ...state, status: 'request' };
     },
     [asyncRequestSuccess]: (state, payload) => {
-        return { ...state, status: 'success', count:200 };
+        return { ...state, status: 'success',count: 200, body:payload.payload.data.body, title: payload.payload.data.title };
     },
     [asyncRequestFailure]: (state, payload) => {
-        return { ...state, status: 'fail', count:404 };
+        return { ...state, status: 'fail', count:404, body:'', title: '' };
     },
 }, initialState);
 
